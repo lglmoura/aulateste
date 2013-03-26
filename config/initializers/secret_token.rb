d3f4645cb5b544c34e9ff38ad48a8238950e08f5
Luiz Gustavo <lglmoura@gmail.com>
2013-03-26 10:07:23 -0300

    Inicio do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ateste::Application.config.secret_token = '7d0bc1a292a380c77ed5fc71464c38ed1bc841f2391f15957993ea9e966b9c893ab24cfcf9f2566687f5bacc8d67114602a5dae8782934ce5bd51c1a80f30236'
